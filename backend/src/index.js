dd21cb7ab14494018a45ae6a1e535e94df5079af
alex <z5588771@ad.unsw.edu.au>
2025-06-23 22:15:50 +1000

    added backend for frontend framework

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import swaggerJsDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import {connectDB} from "./lib/db.js";
 
@@ -40,6 +41,10 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
